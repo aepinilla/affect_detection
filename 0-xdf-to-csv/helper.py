7001383e2f88562faccb3bdc1f10be2780f4a9c6
Andres Pinilla <apinillapalacios@gmail.com>
2021-12-30 12:08:56 -0500

    xdf-to-csv

---
+++ 0-xdf-to-csv/helper.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def column (matrix, i):
+    return np.array(matrix)[:, i]
+
+exp_participant_codes = ['3I6EY',
+                         '6HARB',
+                         'AY9SI',
+                         'DODE8',
+                         'GLJO8',
+                         'GNIE1',
+                         # 'J7BUL', Corrupted data
+                         # 'JB584', Corrupted data
+                        'KNY2Z',
+                        'MJC27',
+                        'MRB58',
+                        'NJL7V',
+                        'PJGHY',
+                        'QPLQF',
+                        'RSC25',
+                        'SDE14',
+                        'SWLFB',
+                        'SXZNO',
+                        'TXNOY',
+                        'UVBY3',
+                        'VHY9N',
+                        # 'Y8ZJQ', Corrupted data
+                        # 'YOO7M' Outlier
+                        ]
